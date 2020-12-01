00d2c76149bf54a1b6df4da3f35a8d61573e43c3
visuve <veli-matti.visuri@iki.fi>
2020-12-01 22:32:36 +0200

    Use std::byte & fix a bug

--- entropy_calc/entropy_calc.cpp
+++ entropy_calc/entropy_calc.cpp
@@ -1,7 +1,6 @@
 #include <chrono>
 #include <cmath>
 #include <cstdint>
-#include <cuchar>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
@@ -10,19 +9,21 @@
 
 namespace
 {
-	double entropy(std::basic_istream<char8_t>& input_stream)
+	double entropy(std::basic_istream<std::byte>& input_stream)
 	{
-		std::vector<char8_t> buffer(0x10000);
-		std::map<char8_t, uint64_t> frequencies;
+		std::vector<std::byte> buffer(0x10000);
+		std::map<std::byte, uint64_t> frequencies;
 		std::streamsize total_bytes_read = 0;
 
-		while (input_stream.read(buffer.data(), buffer.size()))
+		while (input_stream)
 		{
+			input_stream.read(buffer.data(), buffer.size());
+
 			std::streamsize bytes_read = input_stream.gcount();
 
 			for (std::streamsize i = 0; i < bytes_read; ++i)
 			{
-				char8_t character = buffer[i];
+				std::byte character = buffer[i];
 				++frequencies[character];
 			}
 
@@ -59,7 +60,7 @@ int main(int argc, char** argv)
 			return ENOENT;
 		}
 
-		std::basic_ifstream<char8_t> input_stream(input_path, std::ios::binary);
+		std::basic_ifstream<std::byte> input_stream(input_path, std::ios::binary);
 
 		if (!input_stream)
 		{
@@ -67,18 +68,18 @@ int main(int argc, char** argv)
 			return ENOENT;
 		}
 
-
 		std::cout << input_path << '\t' << entropy(input_stream) << std::endl;
-		return 0;
 	}
 	catch (const std::ios::failure& e)
 	{
 		std::cerr << "An I/O excetion occurred: " << e.what() << std::endl;
+		return EIO;
 	}
 	catch (const std::exception& e)
 	{
 		std::cerr << "An exception occurred: " << e.what() << std::endl;
+		return -1;
 	}
 
-	return EIO;
+	return 0;
 }
