dfcbd8c203e1b103de7f62738af53bab6ffde448
visuve <veli-matti.visuri@iki.fi>
2021-03-04 19:50:46 +0200

    Improve sloth_copy to work better with binary files
    - Read the source data in randomly sized chunks
    - If the source data had no spaces, sloth_copy would not have worked as intended

--- sloth_copy/sloth_copy.cpp
+++ sloth_copy/sloth_copy.cpp
@@ -1,9 +1,12 @@
 #include <atomic>
+#include <array>
 #include <chrono>
 #include <csignal>
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <random>
+#include <span>
 #include <string>
 #include <thread>
 
@@ -16,11 +19,64 @@ namespace
 		g_signal = signal;
 	}
 
+	template<typename T, typename N>
+	T random_numeric_value(
+		N min = std::numeric_limits<N>::min(),
+		N max = std::numeric_limits<N>::max())
+	{
+		thread_local std::random_device device;
+		thread_local std::mt19937_64 engine(device());
+		thread_local std::uniform_int_distribution<N> distribution(min, max);
+
+		return T(distribution(engine));
+	}
+
 	template <typename T>
 	T since_epoch()
 	{
 		auto now = std::chrono::system_clock::now();
 		return std::chrono::duration_cast<T>(now.time_since_epoch());
+	}
+
+	template <size_t N>
+	std::streamsize read_random_number_of_bytes(std::ifstream& input, std::array<char, N>& buffer)
+	{
+		const auto bytes_to_read =
+			random_numeric_value<std::streamsize>(size_t(1), buffer.size());
+
+		input.read(buffer.data(), bytes_to_read);
+
+		return input.gcount();
+	}
+
+	void sleep_for_random_time()
+	{
+		const auto sleep_time = random_numeric_value<std::chrono::milliseconds>(1, 20);
+
+		std::cout << since_epoch<std::chrono::seconds>().count()
+			<< " sleeping for " << sleep_time.count() << "ms..." << std::endl;
+
+		std::this_thread::sleep_for(sleep_time);
+	}
+
+	bool write_and_flush(std::ofstream& output, std::span<char> buffer)
+	{
+		if (!output.write(buffer.data(), buffer.size_bytes()))
+		{
+			std::cerr << "Failed to write!" << std::endl;
+			return false;
+		}
+
+		if (!output.flush())
+		{
+			std::cerr << "Failed to flush!" << std::endl;
+			return false;
+		}
+
+		std::cout << since_epoch<std::chrono::seconds>().count()
+			<< " wrote " << buffer.size_bytes() << "bytes" << std::endl;
+
+		return true;
 	}
 
 	int sloth_copy(
@@ -47,27 +103,26 @@ namespace
 				return EIO;
 			}
 
-			for (std::string line; std::getline(input, line, ' ');)
+			std::array<char, 0x200> buffer;
+
+			while (!g_signal && input)
 			{
+				const std::streamsize bytes = read_random_number_of_bytes(input, buffer);
+
+				if (bytes <= 0)
+				{
+					return EIO;
+				}
+
 				if (g_signal)
 				{
 					return ECANCELED;
 				}
 
-				const auto sleep_time = std::chrono::milliseconds(line.length() * 100);
+				sleep_for_random_time();
 
-				std::cout
-						<< since_epoch<std::chrono::seconds>().count()
-						<< " sleeping for "
-						<< sleep_time.count() << "ms..."
-						<< std::endl;
-
-				std::this_thread::sleep_for(sleep_time);
-				output << line << ' ';
-
-				if (!output.flush())
+				if (!write_and_flush(output, { buffer.begin(), static_cast<size_t>(bytes) }))
 				{
-					std::cerr << "Failed to flush!" << std::endl;
 					return EIO;
 				}
 			}
@@ -107,8 +162,8 @@ int main(int argc, char** argv)
 		}
 
 		return sloth_copy(argv[1], argv[2]);
-
-	} catch (const std::exception& e)
+	}
+	catch (const std::exception& e)
 	{
 		std::cerr << "An exception occurred: " << e.what() << std::endl;
 	}
